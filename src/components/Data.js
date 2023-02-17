721dda7f3968097fee9e697bbe8e16dc4d7dc417
sufia-wali <sufiawali1998@gmail.com>
2023-02-17 17:02:57 +0530

    second commit

---
+++ src/components/Data.js
@@ -0,0 +1,458 @@
+const Data= {
+  "todos": [
+    {
+      "id": 1,
+      "title": "Clean Restroom",
+      "description": "The restroom needs to be cleaned to ensure that it is hygienic and safe for use.",
+      "dueDate": "2023-02-24",
+      "tags": ["Cleaning", "Restroom", "Maintenance"],
+      "status": "OPEN"
+    },
+    {
+      "id": 2,
+      "title": "Buy Groceries",
+      "description": "Purchase groceries for the week",
+      "dueDate": "2023-02-22",
+      "tags": ["Groceries", "Shopping"],
+      "status": "OPEN"
+    },
+    {
+      "id": 3,
+      "title": "Finish Project Proposal",
+      "description": "Complete the project proposal and submit it to the client.",
+      "dueDate": "2023-03-01",
+      "tags": ["Project", "Work", "Deadline"],
+      "status": "WORKING"
+    },
+    {
+      "id": 4,
+      "title": "Call Mom",
+      "description": "Check in with mom and catch up on family news.",
+      "dueDate": "2023-02-18",
+      "tags": ["Family", "Personal"],
+      "status": "DONE"
+    },
+    {
+      "id": 5,
+      "title": "Run 5 Miles",
+      "description": "Go for a run and complete 5 miles.",
+      "dueDate": "2023-02-21",
+      "tags": ["Exercise", "Health", "Personal"],
+      "status": "OVERDUE"
+    },
+    {
+      "id": 6,
+      "title": "Pay Rent",
+      "description": "Submit payment for the monthly rent.",
+      "dueDate": "2023-02-28",
+      "tags": ["Finance", "Bills", "Deadline"],
+      "status": "OPEN"
+    },
+    {
+      "id": 7,
+      "title": "Organize Closet",
+      "description": "Sort through clothes and reorganize closet",
+      "dueDate": "2023-02-28",
+      "tags": ["Cleaning", "Organization"],
+      "status": "OPEN"
+    },
+    {
+      "id": 8,
+      "title": "Plan Vacation",
+      "description": "Research and plan vacation destination and activities",
+      "dueDate": "2023-03-31",
+      "tags": ["Travel", "Relaxation"],
+      "status": "WORKING"
+    },
+    {
+      "id": 9,
+      "title": "Attend Doctor's Appointment",
+      "description": "See the doctor for a checkup and get lab work done",
+      "dueDate": "2023-03-15",
+      "tags": ["Health", "Appointment"],
+      "status": "OPEN"
+    },
+    {
+      "id": 10,
+      "title": "Submit Monthly Expense Report",
+      "description": "Gather receipts and submit monthly expense report to supervisor",
+      "dueDate": "2023-03-05",
+      "tags": ["Work", "Finance"],
+      "status": "WORKING"
+    },
+    {
+      "id": 11,
+      "title": "Clean Out Refrigerator",
+      "description": "Check expiration dates and discard old food from refrigerator",
+      "dueDate": "2023-02-20",
+      "tags": ["Cleaning", "Food"],
+      "status": "OVERDUE"
+    },
+    {
+      "id": 12,
+      "title": "Buy Groceries",
+      "description": "Make a list and go grocery shopping for the week",
+      "dueDate": "2023-02-22",
+      "tags": ["Shopping", "Food"],
+      "status": "WORKING"
+    },
+    {
+      "id": 13,
+      "title": "Attend Networking Event",
+      "description": "Attend the local networking event and make new contacts",
+      "dueDate": "2023-03-10",
+      "tags": ["Networking", "Social"],
+      "status": "OPEN"
+    },
+    {
+      "id": 14,
+      "title": "Pay Credit Card Bill",
+      "description": "Make a payment on the credit card bill before the due date",
+      "dueDate": "2023-03-01",
+      "tags": ["Finance", "Bills"],
+      "status": "WORKING"
+    },
+    {
+      "id": 15,
+      "title": "Replace Air Filter",
+      "description": "Buy a new air filter and replace the old one in the AC unit",
+      "dueDate": "2023-02-25",
+      "tags": ["Maintenance", "Household"],
+      "status": "OPEN"
+    },
+    {
+      "id": 16,
+      "title": "Schedule Dentist Appointment",
+      "description": "Call and schedule a dentist appointment for a routine checkup",
+      "dueDate": "2023-03-05",
+      "tags": ["Health", "Appointment"],
+      "status": "OPEN"
+    },
+    {
+      "id": 17,
+      "title": "Complete Online Course",
+      "description": "Finish the online course on machine learning",
+      "dueDate": "2023-04-10",
+      "tags": ["Learning", "Online"],
+      "status": "OPEN"
+    },
+    {
+      "id": 18,
+      "title": "Prepare for Job Interview",
+      "description": "Research the company and prepare answers for commonly asked interview questions",
+      "dueDate": "2023-03-15",
+      "tags": ["Career", "Job Search"],
+      "status": "WORKING"
+    },
+    {
+      "id": 19,
+      "title": "Replace Broken Window",
+      "description": "Purchase a new window and replace the broken one in the bedroom",
+      "dueDate": "2023-02-25",
+      "tags": ["Maintenance", "Household"],
+      "status": "OVERDUE"
+    },
+    {
+      "id": 20,
+      "title": "Buy Mother's Day Gift",
+      "description": "Pick out a gift for mother's day and order it online",
+      "dueDate": "2023-05-09",
+      "tags": ["Shopping", "Gifts"],
+      "status": "OPEN"
+    },
+    {
+      "id": 21,
+      "title": "Practice Yoga",
+      "description": "Set aside time for daily yoga practice",
+      "dueDate": "",
+      "tags": ["Fitness", "Mental Health"],
+      "status": "OPEN"
+    },
+    {
+      "id": 22,
+      "title": "Book Flight for Vacation",
+      "description": "Search for flight deals and book a flight for the upcoming vacation",
+      "dueDate": "2023-03-05",
+      "tags": ["Travel", "Vacation"],
+      "status": "WORKING"
+    },
+    {
+      "id": 23,
+      "title": "Organize Closet",
+      "description": "Sort through clothes and organize closet by color and season",
+      "dueDate": "",
+      "tags": ["Organization", "Household"],
+      "status": "OPEN"
+    },
+    {
+      "id": 24,
+      "title": "Attend Gym Class",
+      "description": "Sign up and attend the HIIT class at the local gym",
+      "dueDate": "2023-02-20",
+      "tags": ["Fitness", "Exercise"],
+      "status": "OVERDUE"
+    },
+    {
+      "id": 25,
+      "title": "Submit Taxes",
+      "description": "Gather necessary documents and file taxes before the deadline",
+      "dueDate": "2023-04-15",
+      "tags": ["Finance", "Tax Season"],
+      "status": "OPEN"
+    },
+    {
+      "id": 26,
+      "title": "Volunteer at Local Shelter",
+      "description": "Spend time volunteering at the local animal shelter",
+      "dueDate": "",
+      "tags": ["Community Service", "Volunteering"],
+      "status": "OPEN"
+    },
+    {
+      "id": 27,
+      "title": "Read New Book",
+      "description": "Start reading the latest novel by favorite author",
+      "dueDate": "",
+      "tags": ["Reading", "Leisure"],
+      "status": "OPEN"
+    },
+    {
+      "id": 28,
+      "title": "Renew Driver's License",
+      "description": "Gather necessary documents and renew driver's license at the DMV",
+      "dueDate": "2023-03-25",
+      "tags": ["Government", "Administration"],
+      "status": "WORKING"
+    },
+    {
+      "id": 29,
+      "title": "Attend Networking Event",
+      "description": "Register and attend the local business networking event",
+      "dueDate": "2023-03-10",
+      "tags": ["Career", "Networking"],
+      "status": "WORKING"
+    },
+    {
+      "id": 30,
+      "title": "Clean Gutters",
+      "description": "Clean out leaves and debris from the gutters on the roof",
+      "dueDate": "",
+      "tags": ["Maintenance", "Household"],
+      "status": "OPEN"
+    },
+    {
+      "id": 31,
+      "title": "Learn a New Skill",
+      "description": "Research and start learning a new skill, such as coding or a new language",
+      "dueDate": "",
+      "tags": ["Learning", "Self-Improvement"],
+      "status": "OPEN"
+    },
+    {
+      "id": 32,
+      "title": "Visit Grandparents",
+      "description": "Schedule a visit with grandparents and spend time catching up",
+      "dueDate": "2023-02-25",
+      "tags": ["Family", "Quality Time"],
+      "status": "WORKING"
+    },
+    {
+      "id": 33,
+      "title": "Take Dog to Vet",
+      "description": "Schedule and take the dog to the vet for annual checkup and shots",
+      "dueDate": "2023-03-15",
+      "tags": ["Pets", "Health"],
+      "status": "OPEN"
+    },
+    {
+      "id": 34,
+      "title": "Plan Home Renovations",
+      "description": "Research and plan home renovations, such as painting or replacing flooring",
+      "dueDate": "",
+      "tags": ["Home Improvement", "Design"],
+      "status": "OPEN"
+    },
+    {
+      "id": 35,
+      "title": "Host a Dinner Party",
+      "description": "Invite friends over for a dinner party and plan the menu and decorations",
+      "dueDate": "2023-03-20",
+      "tags": ["Entertaining", "Social"],
+      "status": "WORKING"
+    },
+    {
+      "id": 36,
+      "title": "Research Investment Options",
+      "description": "Research different investment options and make a plan for long-term savings",
+      "dueDate": "",
+      "tags": ["Finance", "Investing"],
+      "status": "OPEN"
+    },
+    {
+      "id": 37,
+      "title": "Organize Closet",
+      "description": "Sort through and organize clothing in the closet, donate or sell items no longer needed",
+      "dueDate": "",
+      "tags": ["Organization", "Household"],
+      "status": "OPEN"
+    },
+    {
+      "id": 38,
+      "title": "Volunteer at Local Shelter",
+      "description": "Find local animal shelter and sign up to volunteer on weekends",
+      "dueDate": "",
+      "tags": ["Volunteering", "Animals"],
+      "status": "OPEN"
+    },
+    {
+      "id": 39,
+      "title": "Take Yoga Class",
+      "description": "Sign up for a yoga class at the local gym and attend once a week",
+      "dueDate": "",
+      "tags": ["Fitness", "Self-Care"],
+      "status": "OPEN"
+    },
+    {
+      "id": 40,
+      "title": "Research Travel Destinations",
+      "description": "Research and plan next travel destination, look for deals and book flights and accommodations",
+      "dueDate": "",
+      "tags": ["Travel", "Adventure"],
+      "status": "OPEN"
+    },
+    {
+      "id": 41,
+      "title": "Renew Passport",
+      "description": "Check the expiration date of passport and renew if necessary",
+      "dueDate": "2023-03-05",
+      "tags": ["Travel", "Documents"],
+      "status": "WORKING"
+    },
+    {
+      "id": 42,
+      "title": "Clean Car",
+      "description": "Schedule and take car for a wash and vacuum, clean out the interior",
+      "dueDate": "",
+      "tags": ["Automotive", "Household"],
+      "status": "OPEN"
+    },
+    {
+      "id": 43,
+      "title": "Read a Book",
+      "description": "Pick a book from the to-read list and read for at least 30 minutes every day",
+      "dueDate": "",
+      "tags": ["Reading", "Self-Improvement"],
+      "status": "OPEN"
+    },
+    {
+      "id": 44,
+      "title": "Start a Side Hustle",
+      "description": "Brainstorm and research potential side hustles to supplement income",
+      "dueDate": "",
+      "tags": ["Career", "Finance"],
+      "status": "OPEN"
+    },
+    {
+      "id": 45,
+      "title": "Take a Cooking Class",
+      "description": "Sign up for a cooking class and learn a new recipe or technique",
+      "dueDate": "2023-03-15",
+      "tags": ["Cooking", "Hobbies"],
+      "status": "WORKING"
+    },
+    {
+      "id": 46,
+      "title": "Plan Birthday Party",
+      "description": "Make a list of guests, choose a venue, and plan activities and decorations for a birthday party",
+      "dueDate": "2023-04-10",
+      "tags": ["Events", "Celebration"],
+      "status": "WORKING"
+    },
+    {
+      "id": 47,
+      "title": "Schedule Dentist Appointment",
+      "description": "Schedule a routine dental check-up and cleaning with a local dentist",
+      "dueDate": "",
+      "tags": ["Health", "Appointments"],
+      "status": "OPEN"
+    },
+    {
+      "id": 48,
+      "title": "Update Resume",
+      "description": "Update resume with recent experience and accomplishments, and apply to at least two jobs per week",
+      "dueDate": "",
+      "tags": ["Career", "Job Search"],
+      "status": "OPEN"
+    },
+    {
+      "id": 49,
+      "title": "Declutter Storage Room",
+      "description": "Sort through items in the storage room and donate or sell items no longer needed",
+      "dueDate": "",
+      "tags": ["Organization", "Household"],
+      "status": "OPEN"
+    },
+    {
+      "id": 50,
+      "title": "Learn a New Language",
+      "description": "Sign up for language classes or use online resources to learn a new language",
+      "dueDate": "",
+      "tags": ["Education", "Self-Improvement"],
+      "status": "OPEN"
+    },
+    {
+      "id": 51,
+      "title": "Complete Home Improvement Project",
+      "description": "Choose a home improvement project to tackle, gather necessary supplies, and complete the project within one week",
+      "dueDate": "",
+      "tags": ["Home Improvement", "DIY"],
+      "status": "OPEN"
+    },
+    {
+      "id": 52,
+      "title": "Schedule Annual Physical",
+      "description": "Schedule a routine physical exam with primary care physician and get bloodwork done if necessary",
+      "dueDate": "",
+      "tags": ["Health", "Appointments"],
+      "status": "OPEN"
+    },
+    {
+      "id": 53,
+      "title": "Complete Online Course",
+      "description": "Choose an online course to take and complete all assignments and assessments within the designated timeframe",
+      "dueDate": "2023-03-31",
+      "tags": ["Education", "Self-Improvement"],
+      "status": "WORKING"
+    },
+    {
+      "id": 54,
+      "title": "Explore a New City",
+      "description": "Plan a weekend trip to a nearby city and explore new attractions and restaurants",
+      "dueDate": "2023-05-01",
+      "tags": ["Travel", "Adventure"],
+      "status": "WORKING"
+    },
+    {
+      "id": 55,
+      "title": "Create a Monthly Budget",
+      "description": "Track all income and expenses for the month and create a budget for future months",
+      "dueDate": "",
+      "tags": ["Finance", "Budgeting"],
+      "status": "OPEN"
+    }
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Data;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,6 @@
 import { useState, Fragment } from 'react';
 import { Layout, Input, Button, Table, Modal, Form, Tag, Select, DatePicker } from 'antd';
-// import moment from 'moment';
-// import DatePicker from "react-datepicker";
-// import 'dayjs/locale/zh-cn';
-// import dayjs from 'dayjs';
-// import enUS from 'antd/locale/en_US';
-// import zhCN from 'antd/locale/zh_CN';
-// // import locale from 'antd/es/date-picker/locale/zh_CN';
-// dayjs.locale('zh-cn');
+import dayjs from 'dayjs';
 
 
 const { Header, Content } = Layout;
@@ -16,10 +9,15 @@ const { Option } = Select;
 
 
 const TodoList = () => {
+
   const [data, setData] = useState([]); // to-do list data structure
   const [modalVisible, setModalVisible] = useState(false); // modal visibility state
   const [form] = Form.useForm(); // Ant Design form for modal
   const [filterTags, setFilterTags] = useState([]);
+  const today = new Date();
+  const [selectedDate, setSelectedDate] = useState(today.toJSON().toString());
+
+  const [flag, setFlag] = useState(false);
 
   const statusOptions = [
     { text: 'OPEN', value: 'OPEN' },
@@ -37,15 +35,12 @@ const TodoList = () => {
 
   function appendTagToLocalStorage(id, newTag) {
     const tags = JSON.parse(localStorage.getItem("tags")) || [];
-    console.log(id, newTag);
     tags.push({ [id]: newTag });
     localStorage.setItem("tags", JSON.stringify(tags));
-
     const flatArray = tags.flatMap(obj => Object.values(obj)[0]);
     const uniqueArray = Array.from(new Set(flatArray));
     setFilterTags(uniqueArray)
     localStorage.setItem("uniqueTags", JSON.stringify(uniqueArray));
-
   }
 
   // function to remove tags if the task is removed
@@ -53,7 +48,6 @@ const TodoList = () => {
     const tags = JSON.parse(localStorage.getItem("tags")) || [];
     const newData = tags.filter((task) => !task.hasOwnProperty(id));
     localStorage.setItem("tags", JSON.stringify(newData))
-
     const nD = JSON.parse(localStorage.getItem("tags")) || [];
     const flatArray = nD.flatMap(obj => Object.values(obj)[0]);
     const uniqueArray = Array.from(new Set(flatArray));
@@ -63,6 +57,7 @@ const TodoList = () => {
 
   // function to add a new task to the list
   const handleAddTask = (values) => {
+
     const newTask = {
       id: `${Date.now().toString(36)}-${Math.random().toString(36).substr(2, 5)}`,
       created: new Date(),
@@ -74,11 +69,10 @@ const TodoList = () => {
     };
     // appending to localstorage
     appendTagToLocalStorage(newTask['id'], newTask['tags'])
-
     localStorage.setItem("todos", JSON.stringify(newTask))
     const newtask = JSON.parse(localStorage.getItem("todos")) || [];
 
-    // localStorage.setItem('tags',newTask['tags'])
+    // setData(updatedData);
     setData([...data, newtask]);
     setModalVisible(false);
     form.resetFields();
@@ -86,18 +80,14 @@ const TodoList = () => {
 
   // function to modify an existing task
   const handleModifyTask = (values) => {
-    console.log("pasing", values);
-    const modifiedTask = {
-      ...values,
-      dueDate: values.dueDate || null,
-    };
-    console.log("mody task", modifiedTask);
-    const newData = data.map((task) =>
-      task.id === modifiedTask.id ? modifiedTask : task
-    );
+    const id = form.getFieldValue().id;
+    const newDataInString = JSON.stringify(form.getFieldValue());
+    const parsedDate = JSON.parse(newDataInString);
+    const newData = data.map((task)=>task.id===id ? parsedDate :task)
     setData(newData);
     setModalVisible(false);
     form.resetFields();
+    setFlag(false);
   };
 
   // function to delete a task
@@ -107,27 +97,8 @@ const TodoList = () => {
     removeTagFromLocalStorage(id)
   };
 
-  //function for tag filter
-  const handleFilterTags = (selectedTags) => {
-    // console.log("selestedTags", selectedTags);
-  };
-
-  // function to handle the opening of the modal for adding a new task or modifying an existing task
-
-
   // columns for the Ant Design table
   const columns = [
-    // {
-    //   title: 'Timestamp created',
-    //   dataIndex: 'created',
-    //   key: 'created',
-    //   sorter: (a, b) => new Date(a.created) - new Date(b.created),
-    //   render: (created) => <span>{moment(created).format('lll')}</span>,
-    //   // render: (created) => {
-    //   //   const date = new Date(created);
-    //   //   return isNaN(date.getTime()) ? created : date.toLocaleString();
-    //   // },
-    // },
     {
       title: 'Timestamp created',
       dataIndex: 'created',
@@ -146,17 +117,6 @@ const TodoList = () => {
       dataIndex: 'description',
       key: 'description',
     },
-    // {
-    //   title: 'Due Date',
-    //   dataIndex: 'dueDate',
-    //   key: 'dueDate',
-    //   sorter: (a, b) => new Date(a.dueDate) - new Date(b.dueDate),
-    //   render: (dueDate) => <span>{moment(dueDate).format('lll')}</span>,
-    //   // render: (dueDate) => {
-    //   //   const date = new Date(dueDate);
-    //   //   return isNaN(date.getTime()) ? dueDate : date.toLocaleString();
-    //   // },
-    // },
     {
       title: 'Due Date',
       dataIndex: 'dueDate',
@@ -195,24 +155,15 @@ const TodoList = () => {
         <Select
           value={status}
         >
-        {statusOptions.map((option)=>{
-          return(
-            <Option key={option.value} value={option.value}>
-            {option.value}
-          </Option>
-          )
-        })}
+          {statusOptions.map((option) => {
+            return (
+              <Option key={option.value} value={option.value}>
+                {option.value}
+              </Option>
+            )
+          })}
         </Select>
       )
-      // render: (status) =>
-      // (
-      //   <Select value={status}>
-      //     <Option value="OPEN">OPEN</Option>
-      //     <Option value="WORKING">WORKING</Option>
-      //     <Option value="DONE">DONE</Option>
-      //     <Option value="OVERDUE">OVERDUE</Option>
-      //   </Select>
-      // )
     },
     {
       title: 'Action',
@@ -231,14 +182,10 @@ const TodoList = () => {
   ];
 
   const handleEdit = (record) => {
-    console.log(record);
-    // form.getFieldDecorator(record)
-    // form.getFieldDecorator('Amount', { initialValue: 'My Value' })
+    setFlag(true);
     form.setFieldsValue(record);
     setModalVisible(true);
   };
-
-
 
 
   return (
@@ -265,7 +212,7 @@ const TodoList = () => {
           onOk={() => form.submit()}
           onCancel={() => setModalVisible(false)}
         >
-          <Form form={form} onFinish={handleAddTask} onFinishFailed={() => { }}>
+          <Form form={form} onFinish={flag ? handleModifyTask: handleAddTask} onFinishFailed={() => { }}>
             <Form.Item
               label="Title"
               name="title"
@@ -276,16 +223,21 @@ const TodoList = () => {
             <Form.Item label="Description" name="description">
               <Input.TextArea />
             </Form.Item>
-            <Form.Item label="Due Date" name="dueDate">
-              <DatePicker/>
+            <Form.Item label="Due Date" name="dueDate" valuePropName='date'>
+              <DatePicker
+                value={form.getFieldValue().dueDate===undefined ? '': dayjs(form.getFieldValue().dueDate)}
+                onChange={(date, dateString) => {
+                  const newDate = JSON.stringify(date);
+                  if (date) {
+                    setSelectedDate(JSON.parse(newDate))
+                  }
+                }}
+              />
             </Form.Item>
-
-
             <Form.Item label="Tags" name="tags">
-              <Select mode="tags" onChange={handleFilterTags}>
+              <Select mode="tags" >
               </Select>
             </Form.Item>
-
             <Form.Item label="Status" name="status">
               <Select>
                 {statusOptions.map((option) => {
